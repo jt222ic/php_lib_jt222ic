58d1c78d234e244692565d4cbc97bd8e09e1f780
jt222ic <jt222ic@student.lnu.se>
2015-11-04 13:08:12 +0000

    hej

---
+++ controller/LogController.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once("Logger.php");
+
+class LogController
+{
+    
+    public function SendingMessage()
+    {
+                    // ska bli true om det innehåller fel info inuti samt
+   
+       $ip = $_SERVER['REMOTE_ADDR'];  
+      $ipadress = array($_SERVER['REMOTE_ADDR']);
+      sort($ipadress);
+      
+      try{
+         
+          throw new Exception("an order has failed");  
+          
+// prova att kasta fel någonting
+      }
+      
+      catch (EXCEPTION $e)
+      { 
+       //  loggthis("fel av någonting",$e);
+        $f = fopen("controller/store.txt", "a+");
+        fwrite($f, "ip : ");
+        fwrite($f,$ip);
+        fwrite($f,loggthis("an order has failed",$e));
+        fwrite($f,"\n");
+        fwrite($f, loggThis("include call trace",true));
+        fwrite($f,"\n");
+        fwrite($f, loggThis("include an object", false, new \Exception("foo exception")));
+        fwrite($f,date("D dS M,Y h:i a"));
+        fwrite($f,"\n");
+        fwrite($f,$e);
+         // ska man göra så? //
+        fclose($f);
+        
+         echoLog();
+         return false;
+         
+        
+      }      
+     
+        
+       
+       
+       
+       
+                // samma princip?//
+           // använder hans interface skit
+      
+      
+
+        // testar //
+      
+     // file_put_ content kan returnera med true
+        //$IP = json_decode(file_get_contents())
+        
+        
+      //  leta efter senaste server
+       
+        
+    }
+    
+    
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once("controller/LogController.php");
+require_once("view/LogView.php");
+require_once("model/LogItem.php");
+require_once("model/LogCollection.php");
+require_once("Logger.php");
+
+
+
+
+$lg = new LogController();
+
+
+echo "<< index här >>";
+
+$lg->SendingMessage();
+
+
+
+//försöker pusha skiten
+
+// sidan laddas inte upp
